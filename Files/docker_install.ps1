0bac7b6e2a83c9652acba47dd1239001b42ec456
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-08-29 17:03:13 +0530

    Update docker_install.ps1

--- Files/docker_install.ps1
+++ Files/docker_install.ps1
@@ -1,6 +1,6 @@
 # Filename: docker_install.ps1
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Confirm:$False
-Install-Package -Name docker -ProviderName DockerMsftProvider  -Confirm:$False
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider  
 Restart-Computer -force
 # end of script
 
