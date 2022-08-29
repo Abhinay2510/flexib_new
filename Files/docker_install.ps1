730899efa6287a34e9bf3f0946f0048c51f1d1fd
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-08-29 17:24:47 +0530

    Update docker_install.ps1

--- Files/docker_install.ps1
+++ Files/docker_install.ps1
@@ -1,6 +1,7 @@
 # Filename: docker_install.ps1
-ECHO Y |Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-ECHO Y |Install-Package -Name docker -ProviderName DockerMsftProvider  
-Restart-Computer -force
+#ECHO Y |Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+E#CHO Y |Install-Package -Name docker -ProviderName DockerMsftProvider  
+#Restart-Computer -force
 # end of script
-
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
+.\install-docker-ce.ps1
