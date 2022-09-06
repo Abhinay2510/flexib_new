c05e34421563f523339db51e0b8b9817cebb526c
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-09-06 12:52:54 +0530

    Create ps-remote.ps1

---
+++ Files/ps-remote.ps1
@@ -0,0 +1,9 @@
+Enable-PSRemoting -Force
+Set-Item wsman:\localhost\client\trustedhosts * -Confirm:$False -Force
+Restart-Service WinRM
+$username = "Administrator"
+$password = ConvertTo-SecureString -String ";9yFGnsXC)b;ak9Q6QlTJ?Rjy;v=H@fW" -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password 
+$session= New-PSSession  -ComputerName 172.31.34.227 -Credential $cred
+Get-PSSession 
+Copy-Item -Path C:\Users\Administrator\Downloads\result.html\* -ToSession $session -Destination 'C:\Users\Administrator\reports'
