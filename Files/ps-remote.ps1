0dee2305e2c00b176ddaf7357e0c64ccc8e61b0a
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-09-07 16:28:44 +0530

    Update ps-remote.ps1

--- Files/ps-remote.ps1
+++ Files/ps-remote.ps1
@@ -3,8 +3,8 @@ Enable-PSRemoting -Force
 Set-Item wsman:\localhost\client\trustedhosts * -Confirm:$False -Force
 Restart-Service WinRM
 $username = "Administrator"
-$password = ConvertTo-SecureString -String "&$sWcT-.EMIqC!UkL8HBtijk5w=CfF(N" -AsPlainText -Force
+$password = ConvertTo-SecureString -String "WxRuwbjSU.%vp6lLL0J6ENcQL(s7p.Bo" -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password 
-$session= New-PSSession  -ComputerName 172.31.9.229 -Credential $cred
+$session= New-PSSession  -ComputerName 172.31.33.86 -Credential $cred
 Get-PSSession 
-Copy-Item -Path C:\Users\Administrator\reports\* -ToSession $session -Destination 'C:\Users\Administrator\Documents'
+Copy-Item -Path "C:\Users\Administrator\testfile\*" -Destination "C:\Users\Administrator\Downloads" -ToSession $session
