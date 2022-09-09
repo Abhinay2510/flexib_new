8d331426827fe4c4276d7b3a9ca75957359d0416
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-09-09 11:10:36 +0530

    Update ps-remote.ps1

--- Files/ps-remote.ps1
+++ Files/ps-remote.ps1
@@ -7,4 +7,4 @@ $password = ConvertTo-SecureString -String "WxRuwbjSU.%vp6lLL0J6ENcQL(s7p.Bo" -A
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password 
 $session= New-PSSession  -ComputerName 172.31.33.86 -Credential $cred
 Get-PSSession 
-Copy-Item -Path "C:\Users\Administrator\testfile\*" -Destination "C:\Users\Administrator\Downloads" -ToSession $session
+Copy-Item -Path "C:\Users\Administrator\testfile\NRTC_JMeter\*" -Destination "C:\Users\Administrator\Downloads" -ToSession $session
